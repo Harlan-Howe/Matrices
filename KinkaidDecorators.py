cf9dc3f0ca95a953d2ed10029e89e079b6235ef7
Harlan Howe <hhowe7@gatech.edu>
2023-02-28 15:21:43 -0600

    Added credit to ArjunCodes.

--- KinkaidDecorators.py
+++ KinkaidDecorators.py
@@ -1,9 +1,10 @@
 import logging
 from typing import Callable, Any
-
 import functools
 
-
+"""
+Based on an example by Arjan of arjancodes.com: https://www.youtube.com/watch?v=QH5fw9kxDQA 
+"""
 def log_start_stop_method(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
