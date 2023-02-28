ecf6a178eb6d4d31030d513cc4e40e3f3c0dfafe
Harlan Howe <hhowe7@gatech.edu>
2023-02-28 15:14:14 -0600

    Added logger decorator to all non-class, student-

--- KinkaidDecorators.py
+++ KinkaidDecorators.py
@@ -1,13 +1,14 @@
 import logging
 from typing import Callable, Any
 
-
+import functools
 
 
 def log_start_stop_method(func: Callable[..., Any]) -> Callable[..., Any]:
+    @functools.wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        logging.info(f"Starting method {func.__name__}")
-        value = func(*args,**kwargs)
+        logging.info(f"Starting method {func.__name__}({args})")
+        value = func(*args, **kwargs)
         logging.info(f"Finishing method: {func.__name__}")
         return value
     return wrapper
