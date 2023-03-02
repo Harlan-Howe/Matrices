07badb73fc552a32c779545087744b572729060e
Harlan Howe <hhowe7@gatech.edu>
2023-03-02 10:45:27 -0600

    decorator now indents based on depth of program stack... good for recursion?

--- KinkaidDecorators.py
+++ KinkaidDecorators.py
@@ -1,16 +1,17 @@
 import logging
 from typing import Callable, Any
 import functools
-
+import traceback
 """
 Based on an example by Arjan of arjancodes.com: https://www.youtube.com/watch?v=QH5fw9kxDQA 
 """
 def log_start_stop_method(func: Callable[..., Any]) -> Callable[..., Any]:
     @functools.wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        logging.info(f"Starting method {func.__name__}({args})")
+        indent = "\t"*len(traceback.extract_stack())
+        logging.info(f"{indent}Starting method {func.__name__}\n({args})")
         value = func(*args, **kwargs)
-        logging.info(f"Finishing method: {func.__name__}")
+        logging.info(f"{indent}Finishing method: {func.__name__}")
         return value
     return wrapper
 
