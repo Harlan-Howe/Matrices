bf5e8416f748a1c9dff1f2ec6284b6c91446b677
Harlan Howe <harlan.howe@kinkaid.org>
2024-03-18 11:38:30 -0500

    Style cleanup

--- Matrices.py
+++ Matrices.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List
+from typing import List, Optional
 
 # ********************************************************************
 #        PART 1
@@ -8,19 +8,19 @@ from KinkaidDecorators import log_start_stop_method
 
 # logging.basicConfig(level=logging.INFO) # simple version to the output console
 logging.basicConfig(level=logging.DEBUG, filename="log.txt", format="%(asctime)s %(levelname)s %(message)s",
-                    datefmt="%b %e, %Y @ %H:%M:%S %p --- ") # more robust, sent to a file called log.txt in your project
+                    datefmt="%b %e, %Y @ %H:%M:%S %p --- ")  # more robust, sent to a file called log.txt in project
+
 
 class Matrix:
     def __init__(self, A_in: List[List]):
         self.mat = A_in
 
-    def shape(self) -> (int,int):
+    def shape(self) -> (int, int):
         """
-        returns a two-element tuple indicating the number of rows and the number of cols of Matrix A
-        :param A_in: an (N x M) matrix
-        :return: a tuple consisting of two numbers: (N,M)
+        returns a two-element tuple indicating the number of rows and the number of cols of Matrix A.
+        :return: a tuple consisting of two numbers: (N,M) for the shape of the self matrix.
         """
-        return (len(self.mat), len(self.mat[0]))
+        return len(self.mat), len(self.mat[0])
 
     def display(self, message: str = ""):
         """
@@ -36,26 +36,28 @@ class Matrix:
             print(self.__repr__())
         print("=" * 80)
 
-    def __repr__(self)->str:
+    def __repr__(self) -> str:
         return self.__str__()
 
     def __str__(self) -> str:
         result = ""
         try:
             for line in self.mat:
-                result+=(f"{line}\n")
+                result += f"{line}\n"
         except TypeError:  # in case this isn't a matrix....
             result = f"Scalar value: {self.mat}"
         return result
 
-    @classmethod  # this means that this is a static "factory" constructor. You say z = Matrix.zeros((5,5)), rather than z.zeros((5,5))
+    @classmethod  # this means that this is a static "factory" constructor. You say z = Matrix.zeros((5,5)),
+    # rather than z.zeros((5,5))
     def zeros(cls, size: (int,
-                          int)) -> 'Matrix':  # note single quotes because this is the class, itself and has not been completely defined yet.
+                          int)) -> 'Matrix':  # note single quotes because this is the class, itself and has not been
+        # completely defined yet.
         """
         makes an N x M matrix consisting only of zeros
-        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros is called.
+        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros() is called.
         :param size: a two-element list or tuple (N x M)
-        :return: a N x M matrix
+        :return: an N x M matrix
         """
         N = size[0]
         M = size[1]
@@ -63,25 +65,29 @@ class Matrix:
         assert N > 0 and M > 0, "N and M must be positive."
         return cls([[0 for col in range(M)] for row in range(N)])
 
-    @classmethod  # this means that this is a static "factory" constructor. You say z = Matrix.ones((5,5)), rather than z.ones((5,5))
+    @classmethod  # this means that this is a static "factory" constructor. You say z = Matrix.ones((5,5)), rather
+    # than z.ones((5,5))
     def ones(cls, size: (int,
-                         int)) -> 'Matrix':  # note single quotes because this is the class, itself and has not been completely defined yet.
+                         int)) -> 'Matrix':  # note single quotes because this is the class, itself and has not been
+        # completely defined yet.
         """
         makes an N x M matrix consisting only of ones
-        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros is called.
+        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros() is called.
         :param size: a two-element list or tuple (N x M)
-        :return: a N x M matrix
+        :return: an N x M matrix
         """
         N = size[0]
         M = size[1]
         assert N > 0 and M > 0, "N and M must be positive."
         return cls([[1 for col in range(M)] for row in range(N)])
 
-    @classmethod #this means that this is a static "factory" constructor. You say z = Matrix.identity((5,5)), rather than z.identity((5,5))
-    def identity(cls,N:int)-> 'Matrix': #note single quotes because this is the class, itself and has not been completely defined yet.
+    @classmethod  # this means that this is a static "factory" constructor. You say z = Matrix.identity((5,5)), rather
+    # than z.identity((5,5))
+    def identity(cls, N: int) -> 'Matrix':  # note single quotes because this is the class, itself, and has not been
+        # completely defined yet.
         """
         creates a diagonal array of ones, (N x N).
-        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros is called.
+        :param cls: the Matrix class, itself, automatically added, like self normally is, when Matrix.zeros() is called.
         :param N:
         :return: an (N x N) matrix
         """
@@ -89,8 +95,6 @@ class Matrix:
         # -------------------------------------------------------
         # TODO: You write this one.
         # I'd suggest that you start by using another method to get a matrix of the right size, and then modify it.
-
-
 
         return Matrix([["identity not yet written"]])  # remove this when you add your code.
         # -------------------------------------------------------
@@ -100,7 +104,7 @@ class Matrix:
     # ********************************************************************
 
     @log_start_stop_method
-    def transpose(self) -> 'Matrix' :
+    def transpose(self) -> 'Matrix':
         """
         creates a new matrix that is A "flipped" on the diagonal axis.
         :return: a (M x N) matrix, that has A's rows and columns swapped.
@@ -124,7 +128,8 @@ class Matrix:
         :param B: a (N x M) matrix
         :return: a new (N x M) Matrix, where (N x M) is the shape of this matrix.
         """
-        assert self.shape() == B.shape(), f"For addition, matrices must have same shape. These are {self.shape()} and {B.shape()}."
+        assert self.shape() == B.shape(), (f"For addition, matrices must have same shape. These are {self.shape()} and "
+                                           f"{B.shape()}.")
         # -------------------------------------------------------
         # TODO: You write this one.
         # Remember, you need to create a new matrix to put the results
@@ -133,7 +138,7 @@ class Matrix:
         # -------------------------------------------------------
 
     @log_start_stop_method
-    def times(self, n) -> 'Matrix':
+    def times(self, n) -> Optional['Matrix']:
         """
         If n is a scalar, creates a new matrix that is the scalar multiple n·self
         otherwise, if n is a Matrix, performs the dot product of self.dot(n).
@@ -148,7 +153,7 @@ class Matrix:
             return None
 
     @log_start_stop_method
-    def scalar_times(self,n:float) -> 'Matrix':
+    def scalar_times(self, n: float) -> 'Matrix':
         # -------------------------------------------------------
         # TODO: You write this one.
 
@@ -156,7 +161,7 @@ class Matrix:
         # -------------------------------------------------------
 
     @log_start_stop_method
-    def matrix_multiply(self, B:'Matrix')->'Matrix':
+    def matrix_multiply(self, B: 'Matrix') -> 'Matrix':
         """
         performs the matrix multiplication of two matrices
         precondition: self is a (N x M) matrix
@@ -168,20 +173,16 @@ class Matrix:
 
         # write an assertion like the one in add() to make sure this is a viable multiplication.
 
-
         # now do the multiplication.
-
-
-
 
         return Matrix([["Matrix Multiply not yet written"]])  # remove this when you add your code.
         # -------------------------------------------------------
 
-    #already finished....
-    def equals(self, B:'Matrix', threshold=0) -> bool:
+    # already finished....
+    def equals(self, B: 'Matrix', threshold=0) -> bool:
         """
-        indicates whether two matrices consist of the same size and contents. If a threshold is provided, then the values in
-        the cells can differ by as much as the threshold and still be considered equal.
+        indicates whether two matrices consist of the same size and contents. If a threshold is provided, then the
+        values in the cells can differ by as much as the threshold and still be considered equal.
         :param B: a (P x Q) matrix
         :param threshold: a scalar, presumably a small one
         :return: whether N == P and M == Q and each item of the two matrices matches to within threshold.
@@ -199,7 +200,7 @@ class Matrix:
         return True
 
     # already finished....
-    def dot(self, B:'Matrix') -> 'Matrix':
+    def dot(self, B: 'Matrix') -> 'Matrix':
         """
         performs the dot product of two matrices that are (N x 1) or (1 x N)
         :param B: A matrix of the same shape as A or A^T
@@ -212,28 +213,29 @@ class Matrix:
         assert A_rows == 1 or A_cols == 1, f"Dot attempted, but self is size {self.shape()}."
         assert B_rows == 1 or B_cols == 1, f"Dot attempted, but B is size {B.shape()}."
 
-
         if A_rows > 1:
             return self.transpose().dot(B.transpose())
-        assert A_cols == B_cols, f"Dot attempted, but self and B incompatible: self is shape {self.shape()} and B is shape {B.shape()}."
+        assert A_cols == B_cols, (f"Dot attempted, but self and B incompatible: self is shape {self.shape()} "
+                                  f"and B is shape {B.shape()}.")
         return self.matrix_multiply(B.transpose()).mat[0][0]
 
     # already finished....
-    def cross_product(self, B:'Matrix') -> 'Matrix':
+    def cross_product(self, B: 'Matrix') -> 'Matrix':
         """
         finds the cross product of two (1 x 3) matrices (or using the first three values of (1 x N) matrices).
-        :param B: a matrix of the same shape as A
+        :param B: a matrix of the same shape as A.
         :return: a new (1 x 3) matrix
         """
-        assert self.shape() == B.shape(), f"For cross product, shapes should be (1x3) - these are {self.shape()} and {B.shape()}."
+        assert self.shape() == B.shape(), (f"For cross product, shapes should be (1x3) - these are {self.shape()} "
+                                           f"and {B.shape()}.")
         if self.shape()[1] == 1:  # checking for a (3 x 1) matrix, in which case, we'll use the transposes.
-            assert self.shape()[0] > 2 , f"self must be at least 3 in one direction. This is {self.shape()}"
+            assert self.shape()[0] > 2, f"self must be at least 3 in one direction. This is {self.shape()}"
             return self.transpose().cross(B.transpose())
         assert self.shape()[1] > 2, f"self must be at least 3 in one direction. This is {self.shape()}"
 
         return Matrix([[self.mat[0][1] * B.mat[0][2] - self.mat[0][2] * B.mat[0][1],
-                self.mat[0][2] * B.mat[0][0] - self.mat[0][0] * B.mat[0][2],
-                self.mat[0][0] * B.mat[0][1] - self.mat[0][1] * B.mat[0][0]],])
+                        self.mat[0][2] * B.mat[0][0] - self.mat[0][0] * B.mat[0][2],
+                        self.mat[0][0] * B.mat[0][1] - self.mat[0][1] * B.mat[0][0]],])
 
     # ********************************************************************
     #        PART 4 (Still in the Matrix class)
@@ -242,12 +244,14 @@ class Matrix:
     def get_minor(self, r: int, c: int) -> 'Matrix':
         """
         returns a smaller array, composed of matrix A, less the given row and column.
+        precondition: self is an N x M matrix.
         :param r: number row to remove. r < N
         :param c: number col to remove. c < M
-        :return: a array ((N-1) x (M-1)) array
+        :return: an array ((N-1) x (M-1)) array
         """
         num_r, num_c = self.shape()
-        assert r > -1 and r < num_r and c > -1 and c < num_c, f"Attempting to remove row {r} and col {c} from matrix of shape {self.shape()}."
+        assert -1 < r < num_r and -1 < c < num_c, \
+            f"Attempting to remove row {r} and col {c} from matrix of shape {self.shape()}."
         rows = []
         for i in range(num_r):
             if i == r:
@@ -264,7 +268,7 @@ class Matrix:
     def determinant(self) -> float:
         """
         calculates the determinant of a square matrix
-        :param A: a (N x N) matrix
+        Precondition: self is a square matrix.
         :return: a scalar
         """
         num_r, num_c = self.shape()
@@ -273,11 +277,8 @@ class Matrix:
         # TODO: You write this one.
         # Note: this one should be recursive.... make use of the get_minor method immediately above this one!
 
-
-
-
-        pass  # remove this when you add your code.
         # -------------------------------------------------------
+        return -1  # remove this when you add your code.
 
     # ********************************************************************
     #        PART 5 (Still in the Matrix class)
@@ -286,7 +287,7 @@ class Matrix:
     def inverse(self) -> 'Matrix':
         """
         calculates the inverse of the given square matrix
-        :param A: an (N x N) matrix
+        precondition: self is a square matrix
         :return: a new (N x N) matrix that is the inverse of A, or None, if there is no inverse.
         """
         num_r, num_c = self.shape()
@@ -294,16 +295,13 @@ class Matrix:
         # -------------------------------------------------------
         # TODO: You write this one.
 
-        # 1) Construct the minor_matrix. (Remember, this is different than just the minor.)
+        # 1) Construct the minor_matrix. (Remember, this is different from just the minor.)
         # Feel free to make this a separate method and call it from here.
-        
 
         # 2) Calculate the determinant, either by calling the determinant() method or by using the minor_matrix (faster)
 
-
-
-        # 3) The inverse is the transpose of the minor matrix, divided by the determinant. Make sure that the determinant
-        #    isn't zero!
+        # 3) The inverse is the transpose of the minor matrix, divided by the determinant. Make sure that the
+        #    determinant isn't zero!
 
         return Matrix([["Inverse not yet written"]])  # remove this when you add your code.
         # -------------------------------------------------------
